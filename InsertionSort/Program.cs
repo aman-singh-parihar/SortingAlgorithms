26fceb9f349290aa2ac22266288e70fd862b9755
Aman <amanparihar16@gmail.com>
2023-11-30 13:45:22 +0530

    insertion sort algorithm

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -12,45 +12,33 @@ namespace InsertionSort
             {
                 Console.WriteLine(ints[i]);
             }
-            /*
-             12, 45, 23, 51, 18, 8
+/*
+12, 45, 23, 51, 18, 8   12, 45, 23, 51, 18, 8   12, 23, 45, 51, 18, 8   12, 23, 45, 51, 18, 8
 
-            ---1st iteration---
-            i = 1;
-            j = i-1 =0;
-             12, 45, 23, 51, 18, 8
-
-            --2nd iteration---
-            i = 2;
-            j = i-1 = 1;
-             12, 45, 23, 51, 18, 8
-             12, 45, 45, 51, 18, 8
-             12, 23, 45, 51, 18, 8
-
-            ---4th iteration---
-            i = 4;
-            j = 3
-            12, 23, 45, 51, 18, 8
-            12, 23, 45, 51, 51, 8
-            12, 23, 45, 45, 51, 8
-            12, 23, 23, 45, 51, 8
-            12, 18, 23, 45, 51, 8
-             */
+---1st iteration---     ----2nd iteration---    ----3rd iteration----   ---4th iteration---
+i = 1, j = 0            i = 2, j = 1                                    i = 4, j = 3
+ints[i] => 45 => key    ints[i] => 23 => key                            ints[i] => 18 => key
+ints[j] => 12           ints[j] => 45                                   ints[j] => 51
+12, 45, 23, 51, 18, 8   12, 45, 45, 51, 18, 8                           12, 23, 45, 51, 51, 8
+                        12, 23, 45, 51, 18, 8                           
+                                                                        j => 2
+                                                                        ints[j] => 45  
+                                                                        12, 23, 45, 45, 51, 8
+*/
         }
         static void Sort(ref int[] ints)
         {
             int length = ints.Length;
             for (int i = 1; i < length; i++)
             {
-                for (int j = i - 1; j >= 0; j--)
+                int j = i - 1;
+                int key = ints[i];
+                while (j >= 0 && ints[j] > key) 
                 {
-                    if (ints[j] > ints[j+1]) 
-                    {
-                        int temp = ints[j];
-                        ints[j] = ints[j + 1];
-                        ints[j+1] = temp;
-                    }
+                    ints[j+1] = ints[j];
+                    j--;
                 }
+                ints[j + 1] = key;
             }
         }
     }
